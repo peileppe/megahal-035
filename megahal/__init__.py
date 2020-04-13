3fd83a8ee5ade3873b8f7ff8d5586082420436f7
Robert Huselius <robert@huseli.us>
2020-04-13 23:50:55 +0200

    Add stuff to __all__

--- megahal/__init__.py
+++ megahal/__init__.py
@@ -1,9 +1,10 @@
 from megahal.megahal import (
-    DEFAULT_BRAINFILE, DEFAULT_HARD_TIMEOUT, DEFAULT_ORDER,
+    DEFAULT_BRAINFILE, DEFAULT_HARD_TIMEOUT, DEFAULT_ORDER, DEFAULT_BANWORDS,
     DEFAULT_SOFT_TIMEOUT, Dictionary, MegaHAL, Tree, __version__, Brain
 )
 
 __all__ = [
-    'MegaHAL', 'Dictionary', 'Tree', '__version__', 'DEFAULT_ORDER', 'Brain',
-    'DEFAULT_BRAINFILE', 'DEFAULT_HARD_TIMEOUT', 'DEFAULT_SOFT_TIMEOUT'
+    "MegaHAL", "Dictionary", "Tree", "__version__", "DEFAULT_ORDER", "Brain",
+    "DEFAULT_BRAINFILE", "DEFAULT_HARD_TIMEOUT", "DEFAULT_SOFT_TIMEOUT",
+    "DEFAULT_BANWORDS",
 ]
